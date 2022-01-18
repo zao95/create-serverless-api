32579b0e5127a98249bbe0178910663decf2b829
sri02075 <sri02075@naver.com>
2022-01-18 22:20:37 +0900

    ENH: add sample code

---
+++ templates/create-serverless-api/src/api/example.ts
@@ -0,0 +1,19 @@
+
+import { getCake } from '../modules/common'
+
+export const handler = async (event: any, context: any) => {
+	const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+			message: `i get ${getCake()}!`
+		}),
+    }
+    
+    return response
+}
+
+// 1. API 별 필요한 모듈들만...
+//  - 
---
+++ templates/create-serverless-api/src/modules/common.ts
@@ -0,0 +1,4 @@
+
+export const getCake =  () => {
+    return 'cake'
+}
