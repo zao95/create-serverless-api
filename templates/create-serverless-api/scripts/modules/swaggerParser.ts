f4b8892e97fda4dadd43d4eb0e69ef4363a26f8c
sri02075 <sri02075@naver.com>
2022-03-01 23:03:44 +0900

    ENH: update logic

--- templates/create-serverless-api/scripts/modules/swaggerParser.ts
+++ templates/create-serverless-api/scripts/modules/swaggerParser.ts
@@ -1,29 +1,51 @@
 import SwaggerParser from '@apidevtools/swagger-parser'
 
-export const parse = async (swaggerPath: string): Promise<any> => {
+export const parse = async (swaggerPath: string): Promise<any> => { // method key와 resource key가 구분되게끔 분리.
     const swagger = await SwaggerParser.parse(swaggerPath)
     if(!swagger?.paths){
         throw 'no paths definition'
     }
 
     const paths = JSON.parse(JSON.stringify(swagger.paths))
-	const resources = Object.keys(paths).map(pathKey => {
+	const resourceInfos = Object.keys(paths).map(pathKey => {
 		const resourcePath = pathKey.endsWith('/') ? `root${pathKey}` : `root${pathKey}/`
-		const resource = resourcePath.split('/')
-		resource.pop()
-		return { info: paths[pathKey], resource }
+		const resources = resourcePath.split('/')
+		resources.pop()
+		return { info: paths[pathKey], resources }
 	})
-	console.log('re: ', resources)
-	// let resourceTree = {}
-
-	for (const { info, resource } of resources){
+	
+	let resourceTree = {}
+	for (const { info, resources } of resourceInfos){
 		for (const methodKey in info){
-			const method = 
+			const method = info[methodKey]
+			console.log(method, resources)
+
+			let treePeeker: any = resourceTree
+			for (const resource of resources) {
+				const resourceKey = resource.match(/^\{.+\}$/) ? '{path}' : resource
+				if(!treePeeker[resourceKey]){
+					treePeeker[resourceKey] = { children: {}, methods: {} }
+				}
+				treePeeker = treePeeker[resourceKey]
+			}
+
+			treePeeker[methodKey] = JSON.parse(JSON.stringify(method))
+			treePeeker[methodKey].name = 
+			`${methodKey}${
+				resources.slice(1, resources.length)
+				.map(str => 
+					str.replace(/\{/g, '').replace(/\}/g, '')
+					.replace(/\b[a-z]/, letter => letter.toUpperCase())
+				)
+				.join('')
+			}`
 		}
-		pathKey.endsWith('/')
-		const resourceArray = pathKey.replace('/', 'root/').split('/')
-		console.log('key: ', resourceArray)
+		// pathKey.endsWith('/')
+		// const resourceArray = pathKey.replace('/', 'root/').split('/')
+		// console.log('key: ', resourceArray)
 	}
+	console.log(JSON.stringify(resourceTree, null, 4))
+	return resourceTree
 }
 
 export default {
